957a20db5b1d29da48981b0663351b13745a528b
e-lysanov <e.lysanov@yandex.ru>
2023-01-25 18:43:30 +0300

    добавил задачу 6

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         task3();
         task4();
         task5();
+        task6();
     }
 
     public static void task1() {
@@ -77,5 +78,26 @@ public class Main {
         short white = (short) (classes * 2);
         short brown = (short) (classes * 4);
         System.out.println("В школе, где " + classes + " классов, нужно " + white + " банок белой краски и " + brown + " банок коричневой краски");
+    }
+    public static void task6() {
+        System.out.println("Задача 6");
+        // Пишем код для задачи 6
+        byte bananas = 5;
+        short weightBanana = 80;
+        // 400 бананы, 210 молоко, 200 мороженое, 280 яца, всего 1090 = 1.09 кг
+        short generalWeightBananas = (short) (bananas * weightBanana);
+        short milk = 200;
+        short oneHundredMilk = (short) (milk / 100);
+        short weightMilk = 105;
+        short generalWeightMilk = (short) (oneHundredMilk * weightMilk);
+        byte iceCreamPieces = 2;
+        short iceCreamOnePieceWeight = 100;
+        short generalWeightIceCream = (short) (iceCreamPieces * iceCreamOnePieceWeight);
+        byte eggs = 4;
+        short oneEggWeight = 70;
+        short generalWeightEggs = (short) (eggs * oneEggWeight);
+        short generalWeightG = (short) (generalWeightEggs + generalWeightBananas + generalWeightMilk + generalWeightIceCream);
+        double generalWeightK = (generalWeightG / 1000) + ((generalWeightG % 1000) * 0.001);
+        System.out.println("Масса спортзавтрака " + generalWeightG + " граммов или " + generalWeightK + " килограммов");
     }
 }
